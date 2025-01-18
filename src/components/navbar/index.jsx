28c26ee1f49f20c069b8cf559686d55306ce5b71
Lucas Kennde <lucaskennde@gmail.com>
2025-01-18 03:59:04 -0300

    add shadow in button

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,16 +1,24 @@
 import { FileText, UserCircle } from "lucide-react"
 import { Link } from "react-router-dom"
 
-export const Navbar = ({add}) => {
+export const Navbar = ({ add }) => {
     return <>
         <div className="flex justify-between px-14 items-center w-full h-full shadow-md shadow-black border-t-4 text-cyan-700">
-        <Link to={'/dashboard'}><FileText size={60} /></Link>
-        {add && <Link to={'/nova-ordem'}
-        className="flex flex-col text-6xl items-center justify-center rounded-full bg-amber-500 text-zinc-100 w-[100px] h-[100px] translate-y-[-30%] hover:scale-110 transition-transform duration-300">
-            +
-        </Link>}
-        
-        <Link to={'/cliente'}><UserCircle size={60} /></Link>
+            <Link to={'/dashboard'}>
+                <FileText size={60} />
+            </Link>
+            {add && (
+                <Link
+                to={'/nova-ordem'}
+                className="flex flex-col text-6xl items-center justify-center rounded-full bg-amber-500 text-zinc-100 w-[100px] h-[100px] translate-y-[-30%] hover:scale-110 transition-transform duration-300 shadow-[0_-5px_15px_rgba(0,0,0,0.3)]"
+              >
+                +
+              </Link>
+
+            )}
+
+
+            <Link to={'/cliente'}><UserCircle size={60} /></Link>
         </div>
     </>
 }
